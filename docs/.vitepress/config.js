b3cdb6c4eeff628e46995c839803a212798f2cea
abin <596659597@qq.com>
2023-05-31 18:31:59 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  title: '前端开发技术文档',
+  themeConfig: {
+    sidebar: [
+      {
+        text: "技术文档",
+        items: [
+          {
+            text: "前端批量导入数据总结",
+            link: "/technical-documentation/前端批量导入数据总结"
+          },
+          {
+            text: "平台权限使用文档",
+            link: "/technical-documentation/平台权限使用文档"
+          },
+          {
+            text: "中台子项目权限配置",
+            link: "/technical-documentation/中台子项目权限配置"
+          },
+          {
+            text: "中台子项目权限使用",
+            link: "/technical-documentation/中台子项目权限使用"
+          },
+        ]
+      },
+      {
+        text: "开始",
+        items: [
+          {
+            text: "简介",
+            link: "/start/introduction"
+          },
+          {
+            text: "快速上手",
+            link: "/start/快速上手"
+          }
+        ]
+      },
+      {
+        text: "指令",
+        items: [
+          {
+            text: "v-auth",
+            link: "/directive/v-auth"
+          },
+        ]
+      },
+      {
+        text: "基础组件",
+        items: [
+          {
+            text: "Tag 标签",
+            link: "/components/c-tag"
+          },
+        ]
+      },
+      {
+        text: "业务组件",
+        items: [
+          {
+            text: "时区",
+            link: "/components/时区"
+          },
+          {
+            text: "服务器",
+            link: "/components/服务器"
+          },
+        ]
+      },
+    ]
+  }
+};
